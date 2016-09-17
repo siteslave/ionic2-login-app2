84982e224f89e6be624dbc7ee5301313d316c594
Satit Rianpit <rianpit@gmail.com>
2016-09-17 20:28:38 +0700

    add geoloction

--- app/pages/maps/maps.ts
+++ app/pages/maps/maps.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import {GoogleMap, GoogleMapsEvent} from 'ionic-native'
+import {GoogleMap, GoogleMapsEvent, GoogleMapsLatLng, Geolocation} from 'ionic-native'
 
 @Component({
   templateUrl: 'build/pages/maps/maps.html',
 })
 export class MapsPage implements OnInit {
   map: GoogleMap
+  latLng: GoogleMapsLatLng
+
   constructor(private navCtrl: NavController) {
 
   }
@@ -22,6 +24,24 @@ export class MapsPage implements OnInit {
           'zoom': true
         }
       });
+
+      this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
+
+        this.map.setZoom(16)
+        this.map.setMapTypeId('MAP_TYPE_HYBRID')
+        // this.map.refreshLayout()
+
+        Geolocation.getCurrentPosition().then((resp) => {
+          console.log(resp)
+          this.latLng = new GoogleMapsLatLng(resp.coords.latitude, resp.coords.longitude);
+          this.map.setCenter(this.latLng);
+        }, err => {
+          console.log(err)
+        });
+        
+      });
+
+
     }, err => {
       console.log(err)
     });
