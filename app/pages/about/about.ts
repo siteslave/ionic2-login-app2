cd16892006486973eca2c047d9287e4d9432351d
Satit Rianpit <rianpit@gmail.com>
2016-09-18 16:45:33 +0700

    success camera

--- app/pages/about/about.ts
+++ app/pages/about/about.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import {Camera, CameraOptions} from 'ionic-native'
+
 @Component({
   templateUrl: 'build/pages/about/about.html'
 })
 export class AboutPage {
+
+  img: string
   constructor(public navCtrl: NavController) {
+  }
+
+  takePicture() {
+    let options: CameraOptions = {
+      destinationType: 0,
+      quality: 50,
+      targetHeight: 400,
+      targetWidth: 400
+    }
+    Camera.getPicture(options)
+      .then(imageData => {
+      this.img = 'data:image/jpeg;base64,' + imageData;
+    })
+
+  }
+
+  getPicture() {
+    let options: CameraOptions = {
+      destinationType: 0,
+      sourceType: 0,
+      quality: 50,
+      targetHeight: 400,
+      targetWidth: 400
+    }
+    Camera.getPicture(options)
+      .then(imageData => {
+      this.img = 'data:image/jpeg;base64,' + imageData;
+    })
+
   }
 }
