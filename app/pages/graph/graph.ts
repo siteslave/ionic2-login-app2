32edeb43bff2531130c34f99afbd5790792adfd6
Satit Rianpit <rianpit@gmail.com>
2016-09-18 10:38:07 +0700

    remove credit

--- app/pages/graph/graph.ts
+++ app/pages/graph/graph.ts
@@ -24,60 +24,62 @@ export class GraphPage {
       title: { text: 'simple chart' },
       series: [{
         data: [29.9, 71.5, 106.4, 129.2],
-      }]
+      }],
+      credits: false
     };
 
     this.options2 = {
-              chart: {
-            type: 'column'
-        },
+      chart: {
+        type: 'column'
+      },
+      credits: false,
 
+      title: {
+        text: 'Total fruit consumtion, grouped by gender'
+      },
+
+      xAxis: {
+        categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+      },
+
+      yAxis: {
+        allowDecimals: false,
+        min: 0,
         title: {
-            text: 'Total fruit consumtion, grouped by gender'
-        },
+          text: 'Number of fruits'
+        }
+      },
 
-        xAxis: {
-            categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
-        },
+      tooltip: {
+        formatter: function () {
+          return '<b>' + this.x + '</b><br/>' +
+            this.series.name + ': ' + this.y + '<br/>' +
+            'Total: ' + this.point.stackTotal;
+        }
+      },
 
-        yAxis: {
-            allowDecimals: false,
-            min: 0,
-            title: {
-                text: 'Number of fruits'
-            }
-        },
+      plotOptions: {
+        column: {
+          stacking: 'normal'
+        }
+      },
 
-        tooltip: {
-            formatter: function () {
-                return '<b>' + this.x + '</b><br/>' +
-                    this.series.name + ': ' + this.y + '<br/>' +
-                    'Total: ' + this.point.stackTotal;
-            }
-        },
-
-        plotOptions: {
-            column: {
-                stacking: 'normal'
-            }
-        },
-
-        series: [{
-            name: 'John',
-            data: [5, 3, 4, 7, 2],
-            stack: 'male'
+      series: [{
+        name: 'John',
+        data: [5, 3, 4, 7, 2],
+        stack: 'male'
+      }, {
+          name: 'Joe',
+          data: [3, 4, 4, 2, 5],
+          stack: 'male'
         }, {
-            name: 'Joe',
-            data: [3, 4, 4, 2, 5],
-            stack: 'male'
+          name: 'Jane',
+          data: [2, 5, 6, 2, 1],
+          stack: 'female'
         }, {
-            name: 'Jane',
-            data: [2, 5, 6, 2, 1],
-            stack: 'female'
-        }, {
-            name: 'Janet',
-            data: [3, 0, 4, 4, 3],
-            stack: 'female'
+          name: 'Janet',
+          data: [3, 0, 4, 4, 3],
+          stack: 'female'
         }]
     };
   }
