9eb39afdf6d551c708a8489fb9556ec83d13fa7e
Satit Rianpit <rianpit@gmail.com>
2016-09-18 10:25:55 +0700

    add slides

--- app/pages/graph/graph.ts
+++ app/pages/graph/graph.ts
@@ -9,13 +9,76 @@ import {CHART_DIRECTIVES} from 'angular2-highcharts'
 })
 export class GraphPage {
   options: Object
+  options2: Object
+  slideOptions: Object
+
   constructor(private navCtrl: NavController) {
+
+    this.slideOptions = {
+      loop: true,
+      pager: true
+    }
+
     this.options = {
-      chart: {type: 'column'},
+      chart: {type: 'line'},
       title: { text: 'simple chart' },
       series: [{
         data: [29.9, 71.5, 106.4, 129.2],
       }]
+    };
+
+    this.options2 = {
+              chart: {
+            type: 'column'
+        },
+
+        title: {
+            text: 'Total fruit consumtion, grouped by gender'
+        },
+
+        xAxis: {
+            categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+        },
+
+        yAxis: {
+            allowDecimals: false,
+            min: 0,
+            title: {
+                text: 'Number of fruits'
+            }
+        },
+
+        tooltip: {
+            formatter: function () {
+                return '<b>' + this.x + '</b><br/>' +
+                    this.series.name + ': ' + this.y + '<br/>' +
+                    'Total: ' + this.point.stackTotal;
+            }
+        },
+
+        plotOptions: {
+            column: {
+                stacking: 'normal'
+            }
+        },
+
+        series: [{
+            name: 'John',
+            data: [5, 3, 4, 7, 2],
+            stack: 'male'
+        }, {
+            name: 'Joe',
+            data: [3, 4, 4, 2, 5],
+            stack: 'male'
+        }, {
+            name: 'Jane',
+            data: [2, 5, 6, 2, 1],
+            stack: 'female'
+        }, {
+            name: 'Janet',
+            data: [3, 0, 4, 4, 3],
+            stack: 'female'
+        }]
     };
   }
 
